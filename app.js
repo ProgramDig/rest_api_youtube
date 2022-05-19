42ad0bc17db1194aa096ce43906a1fb6921f06b0
Dmitry <63227611+ProgramDig@users.noreply.github.com>
2022-05-19 10:38:38 +0300

    "(create) - REST full API app"

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const path = require('path');
+const {v4} = require('uuid');
+const app = express();
+
+const PORT = 3001;
+
+let CONTACTS = [
+	{id: v4(), name: 'Vlad', value: '+380-68-443-2401', marked: false}
+]
+
+app.use(express.json())
+
+// FULL RESTAPI
+
+// GET
+app.get('/api/contacts', (req, res) => {
+	res.status(200).json(CONTACTS);
+});
+
+// POST
+app.post('/api/contacts', (req, res) => {
+	const contact = {...req.body, id: v4(), marked: false}
+	CONTACTS.push(contact);
+	res.status(201).json(contact);
+});
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+	CONTACTS = CONTACTS.filter(c => c.id !== req.params.id);
+	res.status(200).json({message: 'Contact deleted!'})
+});
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+	const idx = CONTACTS.findIndex(c => c.id === req.params.id);
+	CONTACTS[idx] = req.body;
+	res.status(200).json(CONTACTS[idx]);
+});
+
+app.use(express.static(path.resolve(__dirname, 'client')));
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, 'client', 'index.html'));
+})
+app.listen(PORT, () => {
+	console.log('Server started ...')
+});
